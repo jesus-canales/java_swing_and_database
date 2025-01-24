044e7da2328886123daef2d6f0c4d352c99f98a6
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-23 23:58:13 -0500

    Hemos creado el proyecto, con la estructura de paquetes y también la clase de conexión a la base de datos.

---
+++ StudentApp/src/main/java/com/entrecodigosycafe/studentapp/AppLauncher.java
@@ -0,0 +1,4 @@
+package com.entrecodigosycafe.studentapp;
+
+public class AppLauncher {
+}
---
+++ StudentApp/src/main/java/com/entrecodigosycafe/studentapp/database/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package com.entrecodigosycafe.studentapp.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:33060/studentdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "entrecodigosycafe";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void testConnection() {
+        try (Connection conexion = getConnection()) {
+            if (conexion != null) {
+                System.out.println("✅ Conexion establecida a la base de datos.");
+            }
+        } catch (SQLException e) {
+            System.err.println("❌ Error de conexion");
+        }
+    }
+
+    public static void main(String[] args) {
+        testConnection();
+    }
+}
